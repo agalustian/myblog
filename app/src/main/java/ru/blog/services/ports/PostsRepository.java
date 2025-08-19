4924b6fc7431298de9b529ce82994e70631c80ea
agalustian <agalustian@yahoo.com>
2025-08-20 01:13:43 +0300

    fix (sprint-3): add Posts repository interface

---
+++ app/src/main/java/ru/blog/services/ports/PostsRepository.java
@@ -0,0 +1,22 @@
+package ru.blog.services.ports;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.data.domain.PageRequest;
+import ru.blog.models.PostDetails;
+import ru.blog.models.PostPreview;
+import ru.blog.models.SearchPostsFilter;
+
+public interface PostsRepository {
+  String save(PostDetails postDetails);
+
+  void update(PostDetails postDetails);
+
+  PostDetails getPostDetailsById(String id);
+
+  List<PostPreview> searchPostPreview(SearchPostsFilter searchPostsFilter, PageRequest pageRequest);
+
+  void remove(String postId, String userId);
+
+  Map<String, Integer> findPostsLikesCount(List<String> postIds);
+}
