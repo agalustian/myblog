d2575f03bbef2bc5446677152396c2d6dcb61a64
agalustian <agalustian@yahoo.com>
2025-08-20 01:23:54 +0300

    feat (sprint-3): add test WebConfigurations

---
+++ app/src/test/java/ru/blog/integration/testConfiguration/WebConfiguration.java
@@ -0,0 +1,11 @@
+package ru.blog.integration.testConfiguration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"ru.blog"})
+public class WebConfiguration {
+}
