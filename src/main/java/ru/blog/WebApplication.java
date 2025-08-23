78ca5c1112b814808ce104c747443668f16615c1
agalustian <agalustian@yahoo.com>
2025-08-23 16:18:49 +0300

    feat (sprint-4): migrate to maven from gradle

---
+++ src/main/java/ru/blog/WebApplication.java
@@ -0,0 +1,8 @@
+package ru.blog;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableWebMvc
+@SpringBootApplication
+public class WebApplication {}
