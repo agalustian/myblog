92df6f7906d0b0d59cef9d3ba66c66a81cd723ed
agalustian <agalustian@yahoo.com>
2025-08-25 13:57:58 +0300

    fix (spring-4): remove controller-test profile from PostCommentsControllerTests

--- src/test/java/ru/blog/integration/controllers/PostCommentsControllerTests.java
+++ src/test/java/ru/blog/integration/controllers/PostCommentsControllerTests.java
@@ -10,14 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @TestPropertySource(locations = "classpath:application.yaml")
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("controller-test")
 public class PostCommentsControllerTests {
   private final MockMvc mockMvc;
 
